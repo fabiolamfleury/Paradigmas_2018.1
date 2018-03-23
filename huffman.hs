4b475ee4c915dd202913c79da9a640618b7aa952
Josue Nascimento <josuetk63@gmail.com>
2018-03-22 21:35:55 -0300

    Separating in many modules

--- huffman.hs
+++ huffman.hs
@@ -1,28 +1,4 @@
-import Data.List
 import AbstractData
+import Heap
 
-getFather :: (Int) -> Int
-getFather (x) = div x 2
-
-getSonRight :: Int -> Int
-getSonRight x = 2*x + 1
-
-getSonLeft :: Int -> Int
-getSonLeft x = 2*x
-
-swap :: (Int , Int , [HuffmanNode]) -> [HuffmanNode]
-swap (i, j, xs) | i == j  = xs
-swap (i, j ,xs) |otherwise = left ++ (xs !! b) : middle ++ (xs !! a) : right
-                  where [a,b] = sort[i,j]
-                        left = take a xs
-                        middle = take (b - a - 1) (drop (a + 1) xs)
-                        right = drop (b + 1) xs
-
-
-heapifyUp :: (Int , [HuffmanNode]) -> [HuffmanNode]
-heapifyUp (i, xs) =
-          if v > u then swap(i,f,xs)
-          else xs
-          where f = getFather(i)
-                v = getSecondElement(xs !! i)
-                u = getSecondElement(xs !! f)
+start(i,xs) = heapifyUp(i,xs)
