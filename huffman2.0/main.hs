2fc0147629c1bc88724c67474bfb34acf49fd8d5
Lucas <lucasoliveira2306@gmail.com>
2018-03-29 10:35:33 -0300

    Created sortingList function.
    Signed-off-by: Lucas <lucasoliveira2306@gmail.com>
    Signed-off-by: FabÃola <fabiola.mfleury@gmail.com>
    
    Co-authored-by: Lucas <lucasoliveira2306@gmail.com>
    Co-authored-by: Fabíola <fabiola.mfleury@gmail.com>

--- huffman2.0/main.hs
+++ huffman2.0/main.hs
@@ -2,10 +2,14 @@ module Start where
 import Control.Arrow
 import Data.List
 
-
-start :: String -> IO()
-start text = putStrLn text
+start :: Ord b => [b] -> [(Int, b)]
+start text = sortingList list
+              where list = elementFrequency text
 
 -- Count each char frequency and group it by type (sorting in  ascii order)
-elementFrequency :: Ord a => [a] -> [(a, Int)]
-elementFrequency = map (head &&& length) . group . sort
+elementFrequency :: Ord a => [a] -> [(Int, a)]
+elementFrequency = map (length &&& head) . group . sort
+
+-- Sorting list using type's frequency 
+sortingList :: Ord b1 => [(b1, b)] -> [(b1, b)]
+sortingList list = sortOn fst list
