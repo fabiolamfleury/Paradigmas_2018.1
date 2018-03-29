129bfa45505d8654877616bed679a2f948401e98
Lucas <lucasoliveira2306@gmail.com>
2018-03-29 11:51:32 -0300

    Created HuffTree struct
    Signed-off-by: Lucas <lucasoliveira2306@gmail.com>
    Signed-off-by: Fabíla <fabiola.mfleury@gmail.com>
    
    Co-authored-by: Lucas <lucasoliveira2306@gmail.com>
    Co-authored-by: Fabíola <fabiola.mfleury@gmail.com>

--- huffman2.0/main.hs
+++ huffman2.0/main.hs
@@ -2,6 +2,15 @@ module Start where
 import Control.Arrow
 import Data.List
 
+import Control.Arrow
+import Data.List
+import qualified Data.Map
+import Data.Function
+
+data HuffTree  = Leaf Int Char
+                       | Fork Int HuffTree HuffTree
+                       deriving (Show)
+
 start :: Ord b => [b] -> [(Int, b)]
 start text = sortingList list
               where list = elementFrequency text
@@ -10,6 +19,18 @@ start text = sortingList list
 elementFrequency :: Ord a => [a] -> [(Int, a)]
 elementFrequency = map (length &&& head) . group . sort
 
--- Sorting list using type's frequency 
+-- Sorting list using type's frequency
 sortingList :: Ord b1 => [(b1, b)] -> [(b1, b)]
 sortingList list = sortOn fst list
+
+getFrequency :: HuffTree -> Int
+getFrequency (Leaf frequency _) = frequency
+getFrequency (Fork frequency _ _ ) = frequency
+
+mergeElements :: HuffTree -> HuffTree -> HuffTree
+mergeElements tree1  tree2 = Fork (getFrequency tree1 + getFrequency tree2)  tree1 tree2
+
+ buildTree :: [(Char, Int)] -> HuffTree
+ buildTree = bld . map (uncurry Leaf) . sortBy (compare `on` snd)
+     where  bld (t:[])    = t
+            bld (a:b:cs)  = bld $ insertBy (compare `on` fst) (mergeElements a b) cs
