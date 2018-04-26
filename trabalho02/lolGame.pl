af2e2dec57e44c7ef48d9c03105bd85467ccc488
Josue Nascimento <josuetk63@gmail.com>
2018-04-26 19:12:22 -0300

    Get first character name

--- trabalho02/lolGame.pl
+++ trabalho02/lolGame.pl
@@ -1,8 +1,7 @@
 
-
-a(row(element,_),X):-element.
-thir([Head|Tail]):-a(Head,X).
-get_rows_data(Data):-csv_read_file('champs.csv', Data),thir(Data).
+get_element_row(row(Name,_)):-print(Name).
+get_header([Head|Tail]):-get_element_row(Head).
+get_rows_data(Data):-csv_read_file('dataGame/champs.csv', Data),get_header(Data).
 				 
 
 
