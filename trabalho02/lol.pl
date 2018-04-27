64842335cd84fb627472eff02dc9e5797c711703
Josue Nascimento <josuetk63@gmail.com>
2018-04-27 18:10:24 -0300

    Fix knowledge name import

--- trabalho02/lol.pl
+++ trabalho02/lol.pl
@@ -1,18 +1,18 @@
 :- use_module(library(csv)).
 import_champs:-
-    csv_read_file('knowledge/champs.csv', ChampsData, [functor(champ)]),
+    csv_read_file('knowledgeBase/champs.csv', ChampsData, [functor(champ)]),
     maplist(assert, ChampsData).
 
 import_participants:-
-    csv_read_file('knowledge/participants.csv', ParticipantsData, [functor(participant)]),
+    csv_read_file('knowledgeBase/participants.csv', ParticipantsData, [functor(participant)]),
     maplist(assert, ParticipantsData).
 
 import_stats1:-
-    csv_read_file('knowledge/stats1.csv', Stats1Data, [functor(stats)]),
+    csv_read_file('knowledgeBase/stats1.csv', Stats1Data, [functor(stats)]),
     maplist(assert, Stats1Data).
 
 import_stats2:-
-    csv_read_file('knowledge/stats2.csv', Stats2Data, [functor(stats)]),
+    csv_read_file('knowledgeBase/stats2.csv', Stats2Data, [functor(stats)]),
     maplist(assert, Stats2Data).
 
 import_data:- import_champs(), import_participants(), import_stats1(), import_stats2().
