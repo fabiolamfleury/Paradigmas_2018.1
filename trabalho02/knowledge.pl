605f84b69cd6e81157ef96bd4d7f34fd046f33fe
Josue Nascimento <josuetk63@gmail.com>
2018-04-28 16:33:58 -0300

    Create menu

--- trabalho02/knowledge.pl
+++ trabalho02/knowledge.pl
@@ -1,23 +1,35 @@
 :- use_module(library(csv)).
+/*Import champs.csv*/
 import_champs:-
     csv_read_file('knowledgeBase/champs.csv', ChampsData, [functor(champ)]),
     maplist(assert, ChampsData).
 
+/*Import participants.csv*/
 import_participants:-
     csv_read_file('knowledgeBase/participants.csv', ParticipantsData, [functor(participant)]),
     maplist(assert, ParticipantsData).
 
+/*Import stats1.csv*/
 import_stats1:-
     csv_read_file('knowledgeBase/stats1.csv', Stats1Data, [functor(stats)]),
     maplist(assert, Stats1Data).
 
+/*Import stats1.csv*/
 import_stats2:-
     csv_read_file('knowledgeBase/stats2.csv', Stats2Data, [functor(stats)]),
     maplist(assert, Stats2Data).
 
+/*import all data .csv*/
+import_data:- import_champs(), import_participants(), import_stats1(), import_stats2().
+
+/*lanes game*/
 lane(bot).
 lane(mid).
 lane(top).
 lane(jungle).
 
-import_data:- import_champs(), import_participants(), import_stats1(), import_stats2().
+/*Influence in lanes*/
+influencyInLane(top,mid,jungle).
+influencyInLane(mid,top,suport).
+influencyInLane(bot,top,mid).
+influencyInLane(jungle,top,mid,bot).
--- trabalho02/main.pl
+++ trabalho02/main.pl
@@ -7,16 +7,25 @@ champName:-tab(15),write('Digite o numero do personagem'),nl,nl,
            read(YourChamp),
            champ(Y,YourChamp),
            write('Voce escolheu o personagem: '),
-           write(Y).
+           write(Y),
+           write('\n').
+           write('\n').
 
 opponentInLane:-tab(15),write('Digite o numero do oponente'),nl,nl,
                       read(YourChamp),
                       champ(Y,YourChamp),
                       write('Voce escolheu o personagem: '),
-                      write(Y).
+                      write(Y),
+                      write('\n').
+                      write('\n').
+
 
 lanePlayed:-tab(15),write('Qual lane deseja jogar'),nl,nl,
                 read(LaneGame),
                 lane(LaneGame),
                 write('A lane escolhida foi '),
-                write(LaneGame).
+                write(LaneGame),
+                write('\n').
+                write('\n').
+
+menu:-champName,opponentInLane,lanePlayed.
