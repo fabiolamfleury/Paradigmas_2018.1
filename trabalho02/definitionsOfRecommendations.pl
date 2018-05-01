26eccf4644e74bba684b14cd1f413208c472c1da
Josue Nascimento <josuetk63@gmail.com>
2018-05-01 14:59:52 -0300

    Get Champions in the same match

--- trabalho02/definitionsOfRecommendations.pl
+++ trabalho02/definitionsOfRecommendations.pl
@@ -1,4 +1,16 @@
 :-consult('knowledge.pl').
 
-findParticipants(ChampId,Lane):-findall(IdStats,participant(IdStats,_,ChampId,_,Lane),L),
-                                
+findParticipants(ChampId,Lane,EnemyId):-findall(MatchId,participant(_,MatchId,114,_,Lane),Lista),
+                                        findall(MatchId2,participant(_,MatchId2,24,_,Lane),Lista2),
+                                        findChampsInSameMatch(Lista,Lista2,Matches),write(Matches).
+
+
+
+intersec([], _, []).
+intersec([H1|T1], L2, [H1|Res]) :-
+                    member(H1, L2),
+                    intersec(T1, L2, Res).
+intersec([_|T1], L2, Res) :-
+              intersec(T1, L2, Res).
+
+findChampsInSameMatch(L1,L2,L3):-intersec(L1,L2,L3),!.
