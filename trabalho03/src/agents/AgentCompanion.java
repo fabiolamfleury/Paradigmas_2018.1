0fed2757d1027db939ea9f05d3d9354c8ebf5c06
Josue Nascimento <josuetk63@gmail.com>
2018-06-09 22:32:50 -0300

    Create initial agents

---
+++ trabalho03/src/agents/AgentCompanion.java
@@ -0,0 +1,11 @@
+package agents;
+
+import jade.core.Agent;
+
+public class AgentCompanion extends Agent{
+	
+	protected void setup() {
+		System.out.println("Eu sou o agente companheiro responsavel por");
+	}
+
+}
---
+++ trabalho03/src/agents/AgentTeacher.java
@@ -0,0 +1,12 @@
+package agents;
+
+import jade.core.Agent;
+
+public class AgentTeacher extends Agent{
+	
+	
+	protected void setup() {
+		System.out.println("Eu sou o agente professor");
+	}
+
+}
