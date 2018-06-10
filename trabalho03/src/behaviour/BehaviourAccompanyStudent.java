58f2ddbfb162548f8439236f451a458d56a7454e
Josue Nascimento <josuetk63@gmail.com>
2018-06-09 22:51:18 -0300

    Create initial behaviour for Student

---
+++ trabalho03/src/behaviour/BehaviourAccompanyStudent.java
@@ -0,0 +1,20 @@
+package behaviour;
+
+import jade.core.behaviours.Behaviour;
+
+public class BehaviourAccompanyStudent extends Behaviour{
+	
+	
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean done() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
