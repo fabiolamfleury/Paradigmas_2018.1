da2f0ad79e72c3f1d3acd2399a4eaa7f637a2ed0
Fabíola Fleury <fabiola.mfleury@gmail.com>
2018-06-17 13:02:31 -0300

    Fix teacher meet companions behaviour

--- trabalho03/src/agents/AgentTeacher.java
+++ trabalho03/src/agents/AgentTeacher.java
@@ -1,5 +1,6 @@
 package agents;
 
+
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
@@ -18,16 +19,55 @@ public class AgentTeacher extends Agent{
 	 * 
 	 */
 	private static final long serialVersionUID = 3039621223253454417L;
+	
+	class meetAllCompanionsBehaviour extends OneShotBehaviour { 
+		
+		private static final long serialVersionUID = 7268217846531384442L;
+		
+		private int n = 0; 
+		
+		// Construtor do behaviour. Necessário, pois passamos parâmetro. 
+		public meetAllCompanionsBehaviour(Agent a) { 
+			super(a); 
+		}
 
-	protected void setup() { 
+		// Método action, obrigatório para comportamentos cíclicos.
+		public void action() {
+	        DFAgentDescription dfd = new DFAgentDescription();
+	        ServiceDescription sd  = new ServiceDescription();
+	        sd.setType("AgentCompanion");
+	        dfd.addServices(sd);
+	        
+	        DFAgentDescription[] result;
+			SearchConstraints all = new SearchConstraints();
+			all.setMaxResults(new Long(-1));
+			try {
+				result = DFService.search(this.getAgent(), dfd, all);
+	            System.out.println(result.length + " results" );
+
+				AID[] agents = new AID[result.length];
+	            for (int i=0; i<result.length; i++) {
+	                agents[i] = result[i].getName();
+	                System.out.println(agents[i]);
+	            }
+			} catch (FIPAException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        
+
+		}
 		
+	} 
+	
+	protected void setup() {		
 		System.out.println("Eu sou um professor, e preciso conhecer os Companions dos alunos da minha turma");
-		
+		meetAllCompanionsBehaviour mCB = new meetAllCompanionsBehaviour(this); 
+		addBehaviour(mCB);
 		ServiceDescription sd = new ServiceDescription(); 
 		sd.setName(getName()); 
-		sd.setType("AgentCompanion"); 
+		sd.setType("AgentTeacher"); 
 		register(sd);
-
 	} 
 	
     void register( ServiceDescription sd)
